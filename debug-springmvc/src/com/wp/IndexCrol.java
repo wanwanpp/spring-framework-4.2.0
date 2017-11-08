0ad86e17e4439c287c78841a06d4ccb76ef3b64f
王萍 <wang.03.25@qq.com>
2017-11-08 19:55:51 +0800

    开始debug spring mvc的小demo

--- debug-springmvc/src/com/wp/IndexCrol.java
+++ debug-springmvc/src/com/wp/IndexCrol.java
@@ -1,7 +1,7 @@
 package com.wp;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
@@ -11,11 +11,47 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class IndexCrol {
 
-    @RequestMapping("")
+    @RequestMapping("/index/")
 //    @ResponseBody
     public String getIndex(ModelAndView modelAndView) {
         System.out.println("in");
         return "index";
     }
 
+    @RequestMapping("/param")
+    @ResponseBody
+    public String getParam(@RequestParam("name") String name, @RequestParam("age") Integer age) {
+        String result = "name is " + name + " and age is " + age;
+        System.out.println(result);
+        return result;
+    }
+
+
+    @RequestMapping(value = "/user/{id}", method = RequestMethod.GET)
+    @ResponseBody
+    public String get(@PathVariable("id") Integer id) {
+        System.out.println("get" + id);
+        return "/hello" + id;
+    }
+
+    @RequestMapping(value = "/user/{id}", method = RequestMethod.POST)
+    @ResponseBody
+    public String post(@PathVariable("id") Integer id) {
+        System.out.println("post" + id);
+        return "/hello";
+    }
+
+    @RequestMapping(value = "/user/{id}", method = RequestMethod.PUT)
+    @ResponseBody
+    public String put(@PathVariable("id") Integer id) {
+        System.out.println("put" + id);
+        return "/hello";
+    }
+
+    @RequestMapping(value = "/user/{id}", method = RequestMethod.DELETE)
+    @ResponseBody
+    public String delete(@PathVariable("id") Integer id) {
+        System.out.println("delete" + id);
+        return "/hello";
+    }
 }
